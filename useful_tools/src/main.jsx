176ff1fdd662b97f5c0c244bf010585e6bc04936
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-11 22:58:18 +0200

    chore: refractor for the navigation

--- useful_tools/src/main.jsx
+++ useful_tools/src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+
+const exercices = [
+  {
+    path: '/',
+    element: <div>Homepage</div>,
+  },
+];
+
+
+const router = createBrowserRouter([
+  ...exercices.map(({ path, element }) => ({
+    path,
+    element: (
+      <>
+        <Link to="/">Homepage</Link>
+        <h1>Exercice {path.slice(1)}</h1>
+        {element}
+      </>
+    ),
+  })),
+  {
+    path: '/',
+    element: (
+      <>
+        <h1>Home</h1>
+        <ul>
+          {exercices.map(({ path }) => (
+            <li key={path}>
+              <a href={path}>Exercice {path.slice(1)}</a>
+            </li>
+          ))}
+        </ul>
+      </>
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
